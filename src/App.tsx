e776a1ffde69bfc72b9b74d7c04faeb0232339b3
Ruthvik Uttarala <123519578+Ruthvik-Uttarala@users.noreply.github.com>
2026-08-03 00:04:20 -0400

    Tighten the landing-page flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { AccuracyPromise } from "./components/AccuracyPromise";
-import { Audience } from "./components/Audience";
 import { Deliverables } from "./components/Deliverables";
 import { DemoShowcase } from "./components/DemoShowcase";
 import { FAQ } from "./components/FAQ";
 import { FinalCTA } from "./components/FinalCTA";
 import { Footer } from "./components/Footer";
-import { FounderCard } from "./components/FounderCard";
 import { Hero } from "./components/Hero";
 import { Navigation } from "./components/Navigation";
-import { PortfolioCTA } from "./components/PortfolioCTA";
 import { PricingCard } from "./components/PricingCard";
 import { ProcessTimeline } from "./components/ProcessTimeline";
 import { TrustStrip } from "./components/TrustStrip";
@@ -20,7 +17,7 @@ const jsonLd = {
   url: "https://www.leasereelstudio.com/",
   email: "hello@leasereelstudio.com",
   description:
-    "Done-for-you rental-property video production from approved property photography for rental-property managers.",
+    "Done-for-you property video production from approved listing photography for rental-property managers.",
   areaServed: "United States",
   founder: { "@type": "Person", name: "Ruth" },
   offers: {
@@ -41,12 +38,9 @@ export default function App() {
         <TrustStrip />
         <DemoShowcase />
         <Deliverables />
-        <Audience />
         <ProcessTimeline />
         <AccuracyPromise />
         <PricingCard />
-        <PortfolioCTA />
-        <FounderCard />
         <FAQ />
         <FinalCTA />
       </main>
